9249d18f6c47f8177e75e834538874c91a871a27
abushahed670 <abu.shahed670@gmail.com>
2024-11-20 23:05:19 +0600

    array and length added

---
+++ array.js
@@ -0,0 +1,6 @@
+
+ /**write a array */
+const numbers = [12, 13, 14, 34, 45, 23];
+ const friends =['rahim', 'karim', 'jabbar'];
+ const declair = ['alam', 12, true, false, 'rahim'];
+  console.log(declair.length);
---
+++ length.js
@@ -0,0 +1,2 @@
+const numbers = [12, 13, 14, 23, 24, 34, 35, 45];
+console.log(numbers.length);
