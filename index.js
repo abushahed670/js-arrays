0a45dc2cf3af54cde32b230b98c622f3457e0380
abushahed670 <abu.shahed670@gmail.com>
2024-11-20 23:14:55 +0600

    index added

---
+++ index.js
@@ -0,0 +1,16 @@
+const numbers =[88, 45, 34, 46, 78];
+/**get elements by index */
+// console.log(numbers);
+console.log(numbers[2]);
+//  or 
+
+const fourth = numbers[3];
+console.log(fourth);
+
+//set or update element value by index
+
+let abc = 45;
+
+numbers[1] = 99;
+
+console.log(numbers);
