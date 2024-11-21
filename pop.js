f201e6d4cddeed93c0b92b4231b510eb7ef7c1dd
abushahed670 <abu.shahed670@gmail.com>
2024-11-22 00:24:09 +0600

    pop added

---
+++ pop.js
@@ -0,0 +1,14 @@
+const friends = ['kalam', 'salam', 'jabbar', 'karim', 'pinki'];
+// console.log(friends);
+// friends.pop();
+// console.log(friends);
+
+// if i want to keep the deleted value
+
+console.log(friends);
+const out1 = friends.pop();
+console.log(friends);
+const out2 = friends.pop();
+console.log(friends);
+
+console.log(out1, out2)
