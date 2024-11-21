64eea6878941a5ee2500ae277df35709cfb9638a
abushahed670 <abu.shahed670@gmail.com>
2024-11-22 00:30:16 +0600

    shift and unshift

---
+++ shift-unshift.js
@@ -0,0 +1,10 @@
+const friends = ['kalam', 'salam', 'jabbar', 'karim', 'pinki'];
+// console.log(friends);
+// //delete from first element
+// friends.shift();
+// console.log(friends);
+
+// add a Element in the first 
+console.log(friends);
+friends.unshift('jabbar');
+console.log(friends);
