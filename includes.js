117d9adb220a541913c06b77e4b52d988790a0c7
abushahed670 <abu.shahed670@gmail.com>
2024-11-22 00:38:35 +0600

    includes added

---
+++ includes.js
@@ -0,0 +1,13 @@
+const friends = ['kalam', 'salam', 'jabbar', 'karim', 'pinki'];
+
+console.log(friends.includes('jabbar'));
+console.log(friends.includes('makka'));
+
+// another options to find the includes by if else 
+
+if(friends.includes('jabbar')){
+    console.log('party is enrolling')
+}
+else{
+    console.log('correction first')
+}
